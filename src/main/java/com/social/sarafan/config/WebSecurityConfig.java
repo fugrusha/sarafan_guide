79665b12d3a6b7a029b0059e89fefd82eee5aa54
fugrusha <andrew.golowko@gmail.com>
2020-01-05 20:50:50 +0200

    Init commit Create get, put, delete methods Add google Auth2 Add vue js part

---
+++ src/main/java/com/social/sarafan/config/WebSecurityConfig.java
@@ -0,0 +1,52 @@
+package com.social.sarafan.config;
+
+import com.social.sarafan.domain.User;
+import com.social.sarafan.repository.UserDetailsRepository;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import java.time.LocalDateTime;
+
+@Configuration
+@EnableWebSecurity
+@EnableOAuth2Sso
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .mvcMatchers("/").permitAll()   // allow to see main page for unregistered
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable();
+    }
+
+    // factory bean to be able to map users and messages
+    @Bean
+    public PrincipalExtractor principalExtractor(UserDetailsRepository userDetailsRepository) {
+        return map -> {
+            String id = (String) map.get("sub");    // check if user exists or create new one
+            User user = userDetailsRepository.findById(id).orElseGet(() -> {
+                User newUser = new User();
+
+                newUser.setId(id);
+                newUser.setName((String) map.get("name"));
+                newUser.setEmail((String) map.get("email"));
+                newUser.setGender((String) map.get("gender"));
+                newUser.setLocale((String) map.get("locale"));
+                newUser.setUserpic((String) map.get("picture"));
+
+                return newUser;
+            });
+
+            user.setLastVisit(LocalDateTime.now());
+            return userDetailsRepository.save(user);
+        };
+    }
+}
---
+++ src/main/java/com/social/sarafan/controller/MessageController.java
@@ -0,0 +1,57 @@
+package com.social.sarafan.controller;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import com.social.sarafan.domain.Message;
+import com.social.sarafan.domain.Views;
+import com.social.sarafan.repository.MessageRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("message")
+public class MessageController {
+
+    private final MessageRepository messageRepository;
+
+    @Autowired
+    public MessageController(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    @GetMapping
+    @JsonView(Views.IdName.class)
+    public List<Message> list() {
+        return messageRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Message getMessage(@PathVariable("id") Message message) {
+        return message;
+    }
+
+    @PostMapping
+    public Message createMessage(@RequestBody Message message) {
+        message.setCreationDate(LocalDateTime.now());
+        return messageRepository.save(message);
+    }
+
+    @PutMapping("/{id}")
+    public Message updateMessage(
+            @PathVariable("id") Message msgFromDB,   // get from DB via id
+            @RequestBody Message message             // get from user's updated as json
+    ) {
+        // copy from message to msgFromDB all props except id
+        BeanUtils.copyProperties(message, msgFromDB, "id");
+
+        return messageRepository.save(msgFromDB);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMessage(@PathVariable("id") Message message) {
+        messageRepository.delete(message);
+    }
+}
