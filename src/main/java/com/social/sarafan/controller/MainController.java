97b5d6c0338045afd5ac986b453d1a187c008b83
fugrusha <andrew.golowko@gmail.com>
2020-01-11 00:48:49 +0200

    LazyLoading of pages

--- src/main/java/com/social/sarafan/controller/MainController.java
+++ src/main/java/com/social/sarafan/controller/MainController.java
@@ -5,9 +5,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.social.sarafan.domain.User;
 import com.social.sarafan.domain.Views;
-import com.social.sarafan.repository.MessageRepository;
+import com.social.sarafan.dto.MessagePageDTO;
+import com.social.sarafan.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,16 +19,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.HashMap;
 
+import static com.social.sarafan.controller.MessageController.MESSAGES_PER_PAGE;
+
 @Controller
 @RequestMapping("/")
 public class MainController {
 
-    private final MessageRepository messageRepository;
+    private final MessageService messageService;
     private final ObjectWriter writer;
 
     @Autowired
-    public MainController(MessageRepository messageRepository, ObjectMapper mapper) {
-        this.messageRepository = messageRepository;
+    public MainController(MessageService messageService, ObjectMapper mapper) {
+        this.messageService = messageService;
 
         this.writer = mapper
                 .setConfig(mapper.getSerializationConfig())
@@ -45,8 +50,16 @@ public class MainController {
 
         if (user != null) {
             data.put("profile", user);
-            String messages = writer.writeValueAsString(messageRepository.findAll());   // send as string text, no object
+
+            Sort sort = Sort.by(Sort.Direction.DESC, "id");
+            PageRequest pageRequest = PageRequest.of(0, MESSAGES_PER_PAGE, sort);
+            MessagePageDTO messagePageDTO = messageService.findAll(pageRequest);
+
+            String messages = writer.writeValueAsString(messagePageDTO.getMessages());   // send as string text, no object
+
             model.addAttribute("messages", messages);
+            data.put("currentPage", messagePageDTO.getCurrentPage());
+            data.put("totalPages", messagePageDTO.getTotalPages());
         } else {
             model.addAttribute("messages", "[]");
         }
--- src/main/java/com/social/sarafan/controller/MessageController.java
+++ src/main/java/com/social/sarafan/controller/MessageController.java
@@ -4,50 +4,35 @@ import com.fasterxml.jackson.annotation.JsonView;
 import com.social.sarafan.domain.Message;
 import com.social.sarafan.domain.User;
 import com.social.sarafan.domain.Views;
-import com.social.sarafan.dto.EventType;
-import com.social.sarafan.dto.MetaDTO;
-import com.social.sarafan.dto.ObjectType;
-import com.social.sarafan.repository.MessageRepository;
-import com.social.sarafan.util.WsSender;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import org.springframework.beans.BeanUtils;
+import com.social.sarafan.dto.MessagePageDTO;
+import com.social.sarafan.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.function.BiConsumer;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping("message")
 public class MessageController {
+    public static final int MESSAGES_PER_PAGE = 3;
 
-    private static String URL_PATTERN = "https?:\\/\\/?[\\w\\d\\._\\-%\\/\\?=&#]+";
-    private static String IMAGE_PATTERN = "\\.(jpeg|jpg|gif|png)$";
-
-    private static Pattern URL_REGEX = Pattern.compile(URL_PATTERN, Pattern.CASE_INSENSITIVE);
-    private static Pattern IMG_REGEX = Pattern.compile(IMAGE_PATTERN, Pattern.CASE_INSENSITIVE);
-
-    private final MessageRepository messageRepository;
-    private final BiConsumer<EventType, Message> wsSender;
+    private final MessageService messageService;
 
     @Autowired
-    public MessageController(MessageRepository messageRepository, WsSender wsSender) {
-        this.messageRepository = messageRepository;
-        this.wsSender = wsSender.getSender(ObjectType.MESSAGE, Views.IdName.class);
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
     }
 
     @GetMapping
-    @JsonView(Views.IdName.class)
-    public List<Message> list() {
-        return messageRepository.findAll();
+    @JsonView(Views.FullMessage.class)
+    public MessagePageDTO list(
+            @PageableDefault(size = MESSAGES_PER_PAGE, sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable
+            ) {
+        return messageService.findAll(pageable);
     }
 
     @GetMapping("/{id}")
@@ -60,15 +45,7 @@ public class MessageController {
             @RequestBody Message message,
             @AuthenticationPrincipal User user
     ) throws IOException {
-        message.setCreationDate(LocalDateTime.now());
-        fillMeta(message);
-        message.setAuthor(user);
-
-        Message updatedMessage = messageRepository.save(message);
-
-        wsSender.accept(EventType.CREATE, updatedMessage);
-
-        return updatedMessage;
+        return messageService.create(message, user);
     }
 
     @PutMapping("/{id}")
@@ -76,63 +53,12 @@ public class MessageController {
             @PathVariable("id") Message msgFromDB,   // get from DB via id
             @RequestBody Message message             // get from user's updated as json
     ) throws IOException {
-        // copy from message to msgFromDB all props except id
-        BeanUtils.copyProperties(message, msgFromDB, "id");
-        fillMeta(msgFromDB);
-        Message updatedMessage = messageRepository.save(message);
-
-        wsSender.accept(EventType.UPDATE, updatedMessage);
-
-        return messageRepository.save(msgFromDB);
+        return messageService.update(msgFromDB, message);
     }
 
     @DeleteMapping("/{id}")
     public void deleteMessage(@PathVariable("id") Message message) {
-        messageRepository.delete(message);
-        wsSender.accept(EventType.REMOVE, message);
-    }
-
-    private void fillMeta(Message message) throws IOException {
-        String text = message.getText();
-        // find url in text
-        Matcher matcher = URL_REGEX.matcher(text);
-
-        if (matcher.find()) {
-            // get url from text
-            String url = text.substring(matcher.start(), matcher.end());
-            // check if url is an image
-            matcher = IMG_REGEX.matcher(url);
-            //set link to message
-            message.setLink(url);
-            // if it is an image
-            if (matcher.find()) {
-                message.setLinkCover(url);
-            } else if (!url.contains("youtu")) { // if its youtube video all fields will fill automatically
-                MetaDTO meta = getMeta(url);
-
-                message.setLinkCover(meta.getCover());
-                message.setLinkTitle(meta.getTitle());
-                message.setLinkDescription(meta.getDescription());
-            }
-        }
-    }
-
-    private MetaDTO getMeta(String url) throws IOException {
-        Document doc = Jsoup.connect(url).get();
-
-        Elements title = doc.select("meta[name$=title],meta[property$=title]");
-        Elements description = doc.select("meta[name$=description],meta[property$=description]");
-        Elements cover = doc.select("meta[name$=image],meta[property$=image]");
-
-        return new MetaDTO(
-                getContent(title.first()),
-                getContent(description.first()),
-                getContent(cover.first())
-        );
-    }
-
-    private String getContent(Element element) {
-        return element == null ? "" : element.attr("content");
+        messageService.delete(message);
     }
 
 }
